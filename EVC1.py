f7752c85cd5998cd2bf65f1b3283ca832210bfa0
Martin <mpventer@sun.ac.za>
2019-05-10 13:26:33 +0200

    add in additional translation options

--- EVC1.py
+++ EVC1.py
@@ -113,7 +113,7 @@ class Vermiculus(object):
 
     def _translate_dna(self):
         """
-        read the DNA sequence and build up a string of segment end points.
+        read the DNA sequence and translate it into the a series of segments.
         Returns
         -------
 
@@ -147,8 +147,87 @@ class Radix(Vermiculus):
     """
     Makes a root like structure
     """
-    def __init__(self):
-        self.x = 1
+
+    def __init__(self,
+                 instance_number,
+                 starting_point=np.array([0, 0]),
+                 starting_vector=np.array([0, 1]),
+                 dna_length=60,
+                 unit_length=1.0,
+                 unit_width=1.0,
+                 ):
+        """
+        Initialise a worm
+        Parameters
+        ----------
+        starting_point : array like
+            a 2d vector containing the strarting point for the worm
+        instance_number: int
+            the index for the instance of the worm
+        """
+        self.instance_number = instance_number
+        self.starting_point = starting_point
+        self.starting_vector = starting_vector
+        self.dna_length = dna_length
+        self.unit_length = unit_length
+        self.unit_width = unit_width
+        # self.proteins = {"F": 0.0,
+        #                  "L": -1 * np.pi / 4,
+        #                  "R": np.pi / 4}
+        self.proteins = {"F": 0.0,
+                         "L": 45,
+                         "R": -45}
+        self.amino_acids = list(self.proteins.keys())
+        self.amino_probabilities = [0.4, 0.3, 0.3]
+
+        self.phenotype = [self.starting_point]
+        self.dna = self.transcribe_dna()
+        self.topology = None
+        self.topology_dilated = None
+        self.area = None
+
+    def transcribe_dna(self):
+        """
+        Build a DNA string for the worm that is N elements long by selecting
+        from the available amino acids
+        Returns
+        -------
+            A string N elements long
+        """
+        return ''.join(np.random.choice(self.amino_acids,
+                                        p=self.amino_probabilities
+                                        ) for _ in range(self.dna_length))
+
+    def _translate_dna(self):
+        """
+        read the DNA sequence and translate it into the a series of segments.
+        Returns
+        -------
+
+        """
+        cv = self.starting_vector
+        cp = self.starting_point
+        points = [cp]
+        for acid in self.dna:
+            cp, cv = next_point(self.proteins[acid],
+                                cv,
+                                self.unit_length,
+                                cp)
+            points.append(cp)
+
+        self.phenotype = points
+
+    def build_topology(self):
+        """
+        Translate the phenotype to the topology
+        Returns
+        -------
+
+        """
+        self._translate_dna()
+        self.topology = LineString(self.phenotype)
+        self.topology_dilated = self.topology.buffer(self.unit_width)
+        self.area = self.topology_dilated.area
 
 
 def scan_population(num):
