f4df43ccc905f5d43829954a9230961b0767eb88
martin <mpventer@sun.ac.za>
2019-05-15 18:40:23 +0200

    parallel population evaluaton

--- CreaturePopulationEvaluation.py
+++ CreaturePopulationEvaluation.py
@@ -2,6 +2,9 @@ from Creatures import *
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+from multiprocessing import Pool
+
+import time
 
 def get_individual(gram_length):
     sys = Worm(gram_length)
@@ -10,13 +13,15 @@ def get_individual(gram_length):
     return sys.creature_fitness, sys.l_string
 
 def scan_population(num_individuals, gram_length):
-    fitness = []
-    string = []
-    for _ in tqdm(range(num_individuals)):
-        f, s = get_individual(gram_length)
-        fitness.append(f)
-        string.append(s)
+    start = time.time()
+    with Pool(5) as p:
+        results = p.map(get_individual, [gram_length] * num_individuals)
+    end = time.time()
+    print(end - start)
+    fitness, phenotypes = zip(*results)
     plt.hist(fitness, bins=50)
+    return fitness, phenotypes
 
 
-scan_population(1000, 500)
+if __name__ == '__main__':
+    scan_population(1000, 500)
