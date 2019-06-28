453c76dff27bd607abd097ba0495df7c0670d9a1
ZJouba <zjmonkey.joubert1@gmail.com>
2019-06-28 14:54:22 +0200

    c

--- L_RNN.py
+++ L_RNN.py
@@ -1,13 +1,19 @@
-from tkinter import filedialog
-import tkinter as tk
-import sklearn
-import pickle
-from sklearn.preprocessing import OneHotEncoder
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.layers import LSTM
-from keras.optimizers import RMSprop
+import plaidml.keras
+plaidml.keras.install_backend()
+import plaidml.keras.backend
+
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import RMSprop
+from keras.layers import LSTM, RNN
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+from sklearn.preprocessing import OneHotEncoder
+import pickle
+import sklearn
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+
 
 root = tk.Tk()
 root.withdraw()
@@ -15,25 +21,27 @@ filepath = filedialog.askopenfilename()
 
 data = pickle.load(open(filepath, 'rb'))
 
-data.drop_duplicates(subset='Area', inplace=True)
+data.drop_duplicates(subset='L-string', inplace=True)
 
-data = data[['L-strings', 'Area']]
+data = data[['L-string', 'Area']]
 
-features = data['Area']
-targets = data['L-strings']
+features = data['Area'].values
+targets = data['L-string'].values
 
-encoder = OneHotEncoder()
-enc_targets = encoder.fit_transform(targets)
+encoder = OneHotEncoder(categories='auto', sparse=False)
+enc_targets = encoder.fit_transform(targets.reshape(-1,1)).reshape(2082, 2082, 1)
 
 model = Sequential()
-model.add(LSTM(128, input_shape=(501, data.shape[0])))
-model.add(Dense(data.shape[0]))
-model.add(Activation('softmax'))
+# model.add(LSTM(64, return_sequences=False, dropout=0.1, recurrent_dropout=0.1))
+model.add(Dense(data.shape[0], input_shape=(2082, 2082, 1), activation='softmax'))
+model.add(Dense(501, activation='softmax'))
+model.add(Dense(3, activation='softmax'))
 
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.compile(loss="categorical_crossentropy", optimizer="adam")
 
-model.fit(features, targets, batch_size=128, epochs=5)
+model.fit(features, targets, batch_size=128, epochs=5, verbose=1)
 
 max_predict = model.predict(500)
 max_predict_lstring = encoder.inverse_transform(max_predict)
+
+print(max_predict_lstring)
