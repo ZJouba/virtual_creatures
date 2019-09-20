9efae347489984df2ced5d10e9a3516ab0d81a64
ZJouba <zjmonkey.joubert1@gmail.com>
2019-09-20 09:25:55 +0200

    added n-gram generation

--- gripping.py
+++ gripping.py
@@ -1,20 +1,15 @@
 import numpy as np
+from itertools import product
+
+
+def primordialSoup():
+    grams = []
+    for i in range(2, 6):
+        grams.append(list(product(['TOP', 'BOTTOM'], repeat=i)))
+
+    print()
+
 
 if __name__ == '__main__':
-    """
-    'Extend',
-    'Curl clockwise',
-    'Curl anticlockwise',
-    """
 
-    choices = [
-        'E',
-        'CC',
-        'CA',
-    ]
-
-    generation = []
-
-    while len(generation) <= 500:
-        genotype = [np.random.choice(choices) for _ in range(10)]
-        generation.append(genotype)
+    primordialSoup()
